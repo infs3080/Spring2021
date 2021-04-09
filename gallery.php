ff0c89fb64fd54d7b1a489fae7b8b212b8f17e73
infs3080 <rcook2@uccs.edu>
2021-04-08 19:49:01 -0600

    Updated Gallery page

--- gallery.php
+++ gallery.php
@@ -3,10 +3,39 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- Fancybox Links -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
+    <script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
+
+     <!-- Link to styling for menus and Showcase -->
+     <link rel="stylesheet" href="css/homestyles.css">
+    <link rel="stylesheet" href="css/gallery_style.css">
+    
     <title>GALLERY</title>
 </head>
 <body>
   
+
+    <!-- Navigation buttons-->
+    <nav class="main-nav">
+        <ul>
+            <li><a href="index.html">HOME</a></li>
+            <li><a href="gallery.php">Picture Gallery</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li><a href="reports.php">Reports</a></li>
+        </ul>
+    </nav>
+
+    <!-- Top section with image-->
+    <section >
+        <header class="showcase">
+        <h1>Web Dev is Awesome...</h1>
+        </header>
+    </section>
+
+    <!-- Upload form design -->
     <form action="php/upload.php" method="POST" enctype="multipart/form-data">
 
         <input type="file" name="img_upload">
@@ -14,14 +43,33 @@
     
     </form>
 
-    <?php
-        $files = glob("uploads/*.*");
-        for ($i=0; $i<count($files); $i++)
-        {
-            $num = $files[$i];
-            echo '<img src="'.$num.'" alt="random image">'."&nbsp;&nbsp;";
-            }
-    ?>
+    <!-- Displaying images from our uploads folder -->
+    <section class = "img_gallery">
+        <div class = "gallery_detail">
+            <?php
+                $dir = glob('uploads/*.*');
+                foreach ($dir as $value){
+                
+            ?>
+                <div class = "thumbnails">
+                    <a data-fancybox="gallery" href="<?php echo $value ; ?>"><img src="<?php echo $value ; ?>" alt="<?php echo $value ; ?>"></a>
+                </div>
+            <?php
 
+            
+
+            }
+        
+
+                /* 
+                $files = glob("uploads/*.*");
+                for ($i=0; $i<count($files); $i++)
+                {
+                    $num = $files[$i];
+                    echo '<img src="'.$num.'" alt="random image">'."&nbsp;&nbsp;";
+                    } */
+            ?>
+        </div>
+    </section>
 </body>
 </html>
